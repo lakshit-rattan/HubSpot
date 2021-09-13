5136ac21ba68e596429e75e97553528300419581
Lakshit Rattan <lakshit0439.cse19@chitkara.edu.in>
2021-09-14 00:56:12 +0530

    ...

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+/** It's our own wish that we can write here all the routes that we want. But that is simply not advised to.
+ * Doing so would make the app.js file much much bigger and heavy to load. For that, what we do is, that we make a folder
+ * called 'routes' and then add all the required routes into different files into that folder and then LINK those files to the app.js file.
+ */
 const placeRoutes = require('./routes/places-routes');
 
 const app = express();
--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require("express");
+//Wherever we wish to use express, we HAVE to import the express library from node modules.
 
+//using a special function called router in express
 const router = express.Router();
 
-router.get('/',(req,res,next) => {
-    console.log('GET Request in Places');
-    res.json({message : "It Works fine !"});
+
+//setting the GET method for the '/' path for the router const
+router.get("/", (req, res, next) => {
+  console.log("GET Request in Places");
+  //this is a special method, which sends back a JSON response to wherever it is imported, and whatever written in the json, is parsed accordingly
+  res.json({ message: "It Works fine !" });
 });
 
+//Exporting the const named router. this is the general syntax for exporting.
 module.exports = router;
