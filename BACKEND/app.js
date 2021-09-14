40b72fe6c87992998a785f72e85152aabb4b7f7c
Lakshit Rattan <lakshit0439.cse19@chitkara.edu.in>
2021-09-14 16:21:02 +0530

    dummy segmentation for place-routes

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -9,6 +9,13 @@ const placeRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use(placeRoutes);
-
+//Now there is one thing that we need to take care of. that is, according to the api's that we need, we have to have 2 SET of API's. '/api/places/....' and '/api/users/....' 
+// so we have to add a FILTER on this middleware, such that it is executed only on the path that we specify. so for use(), it is not necessary to give the path filter, but we CAN.
+// This filter that we put, will also not be treated as, the route should be EXACTLY as specified in the filter, but have to just START from it. can be more than that also.
+app.use('/api/places',placeRoutes);
+//now expressJS will only forward requests to our placeRoutes Middleware if their path STARTS from '/api.places/' NOTE - Path can be longer than that also.
+// '/api/places/something' -> VALID
+// '/api' -> INVALID
+// The Route filter that we add in App.js GET method, has to be prepended to the route that is specified here. 
+// so if we want to have GET requests for say,'/api/places', we must not repeat it in app.js, rather take the main filter to app.js and put the filter of whatever is going to AFTER the main filter, i.e, whatever is going to be after '/api/places'. can be anything 
 app.listen(5000);
--- BACKEND/routes/places-routes.js
+++ BACKEND/routes/places-routes.js
@@ -4,12 +4,37 @@ const express = require("express");
 //using a special function called router in express
 const router = express.Router();
 
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous Sky-Scrapers in the World",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
 
 //setting the GET method for the '/' path for the router const
-router.get("/", (req, res, next) => {
+//We actually now want to have a GET request, where the ID is part of the URL. the idea is that we send the request to the API '/places/p1' for eg, and that gives us the data for the place for the ID p1
+//So the ID should be endcoded in the URL(Dynamic ID). To tell express that we have a dynamic segment (which we don't know in advance), we add a colon(:) in our filter and then any identifier of our choice, which is then identified by EXPRESS as a dynammic id and works fine
+//So now, as we know how to succesfully enter into this pid target, we now want to return the place associated with that pid from the database/duummy data
+/**
+ * For that, we need to first get the value that is entered into the dynamic pid compare the value with the values present in the Database/dummy data, and the return the necessary data
+ * ExpressJS covers this too. we get the pid by using the 'Request' object and there we will have a 'params' property  
+ * 'params' -> this property is added by ExpressJS, which in the end, holds an object, where our dynamic segments are stored as KEYS, and it's values, will be the values entered by the user in place of that dynamic segment. eg- {pid:'p1'}
+*/ 
+router.get("/:pid", (req, res, next) => {
+  const placeid = req.params.pid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeid;
+  });
   console.log("GET Request in Places");
   //this is a special method, which sends back a JSON response to wherever it is imported, and whatever written in the json, is parsed accordingly
-  res.json({ message: "It Works fine !" });
+  res.json({place}); // ==> {place} = {place:place} automatically. JS 
 });
 
 //Exporting the const named router. this is the general syntax for exporting.
